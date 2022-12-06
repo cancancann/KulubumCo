08a5e56ba91f3d3bd08ac0717c4036116771df35
cancancann <hikmetcanoyke@outlook.com>
2022-12-06 19:50:39 +0300

    Fix   login cookies catch

--- src/api/index.js
+++ src/api/index.js
@@ -6,7 +6,7 @@ const API = axios.create({ baseURL: BASE_URL });
 const api = {
   auth: {
     register: (data) => API.post('/auth/register', data),
-    login: (data) => API.post('/auth/login', data),
+    login: (data) => API.post('/auth/login', data,{withCredentials: true}),
     forgotPassword: (data) => API.post('/auth/forgotPassword', data),
     resetPassword: (data) => API.post('/auth/', data),
   },
